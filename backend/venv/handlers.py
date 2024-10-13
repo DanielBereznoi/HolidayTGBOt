ff1d54adfdbe89c521e741a7c336d79d552a7646
Daniel Bereznoi <danielbereznoi@gmail.com>
2024-10-13 20:37:49 +0300

    Handlers commit

--- backend/venv/handlers.py
+++ backend/venv/handlers.py
@@ -3,8 +3,9 @@ from telebot import types
 import time
 import datetime
 import event_service
+import saved_token
 
-bot = telebot.TeleBot(token='7966855356:AAEp-siw_TF5kbfpKczM7CyeOnDkazaRGfw')
+bot = telebot.TeleBot(token=saved_token.get_token())
 
 current_transactions = {}
 
@@ -20,6 +21,64 @@ def delete_transactions(keys):
         bot.send_message(key, "Transaction timed out")
         current_transactions.pop(key, None)  # Use pop with default to avoid errors
 
+
+def validate_date(date_string):
+    date_format = "%d.%m.%Y"
+    try:
+        datetime.datetime.strptime(date_string, date_format)
+        return True
+    except ValueError:
+        return False
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, f"Hello {message.chat.username}!")
+    print(message)
+
+
+@bot.message_handler(commands=['addrepeatingevent'])
+def add_repeating_event(message):
+    bot.send_message(message.chat.id, "Insert Holiday name")
+    current_transactions[message.chat.id] = [time.time(), True]
+    print("add new holiday")
+
+
+@bot.message_handler(commands=['addevent'])
+def add_new_event(message):
+    bot.send_message(message.chat.id, "Insert Holiday name")
+    current_transactions[message.chat.id] = [time.time(), False]
+
+
+@bot.message_handler(commands=['deleteevent'])
+def delete_holiday(message):
+    chat_events = event_service.get_events_by_chat_id(message.chat.id)
+    markup = types.InlineKeyboardMarkup()
+    for event in chat_events:
+        markup.add(types.InlineKeyboardButton(text=f'{str(event[1])} - {str(event[2])}',
+                                              callback_data=str(event[3])))
+    bot.reply_to(message, reply_markup=markup, text="Select event you want to delete")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(callback):
+    event_service.delete_data_from_db(callback.data)
+    bot.send_message(callback.message.chat.id, "Event deleted")
+
+@bot.message_handler(commands=['allevents'])
+def all_holidays(message):
+    print("all holidays")
+
+
+@bot.message_handler(commands=['cancel'])
+def cancel(message):
+    current_transactions.pop(message.chat.id)
+    print("cancel")
+
+
+@bot.message_handler(commands=['stop'])
+def stop(message):
+    print("stop")
 
 @bot.message_handler()
 def handle_replies(message):
@@ -43,68 +102,6 @@ def handle_replies(message):
                     bot.send_message(chat_id, error_message)
             else:
                 bot.send_message(chat_id, "Invalid input format. Use 'dd.MM.yyyy - event name'.")
-
-    print(message.text)
-
-
-def validate_date(date_string):
-    date_format = "%d.%m.%Y"
-    try:
-        datetime.datetime.strptime(date_string, date_format)
-        return True
-    except ValueError:
-        return False
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, f"Hello {message.chat.username}!")
-    print(message)
-
-
-@bot.message_handler(commands=['addrepeatingevent'])
-def add_new_occasion(message):
-    bot.send_message(message.chat.id, "Insert Holiday name")
-    current_transactions[message.chat.id] = [time.time(), True]
-    print("add new holiday")
-
-
-@bot.message_handler(commands=['addevent'])
-def add_new_occasion(message):
-    bot.send_message(message.chat.id, "Insert Holiday name")
-    current_transactions[message.chat.id] = [time.time(), False]
-    print("add new holiday")
-
-
-@bot.message_handler(commands=['deleteevent'])
-def delete_holiday(message):
-    chat_events = event_service.get_events_by_chat_id(message.chat.id)
-    markup = types.InlineKeyboardMarkup()
-    for event in chat_events:
-        markup.add(types.InlineKeyboardButton(text=f'{event[1]} - {event[2]}',
-                                              callback_data=event[3]))
-    bot.reply_to(message, reply_markup=markup)
-
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(callback):
-    event_service.delete_data_from_db(callback.data)
-    bot.send_message(callback.message.chat.id, "Event deleted")
-
-@bot.message_handler(commands=['allevents'])
-def all_holidays(message):
-    print("all holidays")
-
-
-@bot.message_handler(commands=['cancel'])
-def cancel(message):
-    current_transactions.pop(message.chat.id)
-    print("cancel")
-
-
-@bot.message_handler(commands=['stop'])
-def stop(message):
-    print("stop")
 
 
 bot.polling(non_stop=True)
