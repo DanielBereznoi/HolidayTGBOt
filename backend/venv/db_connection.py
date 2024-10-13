4c8406e1aafb2ca09650795ff43837347a0c6074
Daniel Bereznoi <danielbereznoi@gmail.com>
2024-10-13 20:44:06 +0300

    Handlers commit

--- backend/venv/db_connection.py
+++ backend/venv/db_connection.py
@@ -4,7 +4,7 @@ def get_connection():
     return psycopg2.connect(
         database="Project_Birthday",
         user='postgres',
-        password='admin',
+        password='postgres',
         host='127.0.0.1',
         port='5432'
     )
