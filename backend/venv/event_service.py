0dfa04cf76363d11c839dd4f2d8a6fbfddd3243c
RUSparrow <sparrow_ivanov@outlook.com>
2024-10-13 16:45:54 +0300

    test

--- backend/venv/event_service.py
+++ backend/venv/event_service.py
@@ -3,11 +3,15 @@ from db_connection import get_connection
 
 def execute_query(query, params=None):
     """Optimisation"""
-    with get_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(query, params)
-            if query.strip().lower().startswith('select'):
-                return cursor.fetchall()
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, params)
+                if query.strip().lower().startswith('select'):
+                    return cursor.fetchall()
+    except psycopg2.Error as e:
+        print(f"Ошибка при выполнении запроса: {e}")
+
 
 def get_data_from_db():
     # Выполняем запрос для получения всех данных из таблицы
@@ -18,15 +22,55 @@ def get_data_from_db():
     for row in rows:
         print(row)
 
-def add_data_to_db(new_data):
-    # SQL-запрос добавления данных
-    execute_query('INSERT INTO "Events" (column1, column2) VALUES (%s, %s)', new_data)
+def add_data_to_db_false(chat_ID, event_date, event_name, repeating=False):
+    # SQL-запрос добавления данных с новым столбцом
+    execute_query('INSERT INTO "Events" ("chat_ID", "event_name", "event_date", "repeating") VALUES (%s, %s, %s, %s)', 
+                  (chat_ID, event_name, event_date, repeating))
+    print("Данные добавлены.")
+    
+def add_data_to_db_true(chat_ID, event_date, event_name, repeating=True):
+    # SQL-запрос добавления данных с новым столбцом
+    execute_query('INSERT INTO "Events" ("chat_ID", "event_name", "event_date", "repeating") VALUES (%s, %s, %s, %s)', 
+                  (chat_ID, event_name, event_date, repeating))
     print("Данные добавлены.")
 
+def check_record_exists(chat_ID, event_date, event_name):
+    # Проверка существования записи
+    query = 'SELECT id FROM "Events" WHERE "chat_ID" = %s AND "event_name" = %s AND "event_date" = %s'
+    result = execute_query(query, (chat_ID, event_name, event_date))
+    return result
+
 def delete_data_from_db(identifier):
-    # SQL-запрос удаления данных по id
-    execute_query('DELETE FROM "Events" WHERE id = %s', (identifier,))
-    print("Данные удалены.")
+    # SQL-запрос удаления данных
+    record = check_record_exists(identifier)
+    if record:
+        execute_query('DELETE FROM "Events" WHERE id = %s', (identifier,))
+        print("Данные удалены.")
+    else:
+        print("Ошибка: Запись не найдена.")
+
+
+def get_events_by_chat_id(chat_ID):
+    # Получение всех событий для пользователя, отсортированных по дате
+    query = 'SELECT * FROM "Events" WHERE "chat_ID" = %s ORDER BY "event_date"'
+    rows = execute_query(query, (chat_ID,))
+    return rows
+
+def get_events_by_today(chat_ID):
+    """Получение всех событий для пользователя за сегодня"""
+    query = '''
+        SELECT * FROM "Events" 
+        WHERE "chat_ID" = %s AND "event_date" = CURRENT_DATE
+    '''
+    rows = execute_query(query, (chat_ID,))
+    
+    print(f"События для Вас {chat_ID} на сегодня:")
+    if rows:
+        for row in rows:
+            print(row)
+    else:
+        print("Нет событий на сегодня для данного Вас.")
+    return rows
 
 # Вызов функции для получения данных
 if __name__ == "__main__":
---
+++ backend/venv/scheduler.py
@@ -0,0 +1,35 @@
+import time
+import schedule
+from events import get_all_events_today  # Импорт новой функции из другого файла
+
+def check_events_daily():
+    """Проверка наличия событий на сегодня и уведомление пользователя."""
+    try:
+        rows = get_all_events_today()
+        if rows:
+            # Логика уведомления пользователя
+            print("Сегодня у вас есть следующие события:")
+            for row in rows:
+                print(row)  # Или другую логику вывода
+        else:
+            print("Сегодня нет событий.")
+    except Exception as e:
+        print(f"Ошибка при проверке событий: {e}")
+
+
+def job():
+    """Работа"""
+    print("Проверка событий на сегодня...")
+    check_events_daily()
+
+def run_scheduler():
+    """Запуск планировщика для проверки каждый час."""
+    schedule.every().hour.do(job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(3500)  # Проверка в сек
+
+# Пример использования
+if __name__ == "__main__":
+    run_scheduler()
